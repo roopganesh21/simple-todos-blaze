c9c557e27400cfc4bd2f85e7d502252c485cf2d3
roopganesh21 <rupganesh21@gmail.com>
2026-06-17 17:35:06 +0530

    feat: implement task creation form

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,3 +8,25 @@ Template.mainContainer.helpers({
     return TasksCollection.find({}, { sort: { createdAt: -1 } });
   },
 });
+
+Template.mainContainer.events({
+  async 'submit form'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value.trim();
+
+    if (text) {
+      await TasksCollection.insertAsync({
+        text,
+        createdAt: new Date(),
+        isChecked: false,
+      });
+
+      // Clear form input
+      target.text.value = '';
+    }
+  },
+});
